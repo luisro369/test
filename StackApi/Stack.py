35960d4114e1e3e8ca6d1f4171821b5661e72693
luisro369 <00005513@uca.edu.sv>
2019-06-21 12:04:32 -0600

    agregando excepciones a la clase

--- StackApi/Stack.py
+++ StackApi/Stack.py
@@ -9,18 +9,24 @@ class Stack():
         self.Title_link = []
     #======================Method 1 ASKING IN STACKOVERFLOW================================
     def ask(self,question):
-        res = requests.get(self.URL_BASE + "/search?q=" + question)
-        soup = BeautifulSoup(res.text,"html.parser")
-        question_summary = soup.findAll("div",{"class": "question-summary search-result"})
-        title = []
-        for question in question_summary:
-            title.append(question.h3.a["title"])
-            self.Title_link.append(question.h3.a["href"])
-        return title
-    #======================Method 2 GETTING THE ANSWER=====================================
+        try:
+            res = requests.get(self.URL_BASE + "/search?q=" + question)
+            soup = BeautifulSoup(res.text,"html.parser")
+            question_summary = soup.findAll("div",{"class": "question-summary search-result"})
+            title = []
+            for question in question_summary:
+                title.append(question.h3.a["title"])
+                self.Title_link.append(question.h3.a["href"])
+            return title
+        except Exception as e:
+            return e
+            #======================Method 2 GETTING THE ANSWER=====================================
     def getAnswer(self,response):
-        res = requests.get(self.URL_BASE + self.Title_link[response])
-        soup = BeautifulSoup(res.text,"html.parser")
-        answer_summary = soup.find("div",{"class": "post-text"})#<---es kann nur "find" sein, wenn du nur die erste frage willst
-        return answer_summary
+        try:
+            res = requests.get(self.URL_BASE + self.Title_link[response])
+            soup = BeautifulSoup(res.text,"html.parser")
+            answer_summary = soup.find("div",{"class": "post-text"})#<---es kann nur "find" sein, wenn du nur die erste frage willst
+            return answer_summary
+        except Exception as e:
+            return e
 
